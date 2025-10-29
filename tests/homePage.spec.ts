be54f482a3073ac6d9f1a51cd9f92ec5011f985f
biocodeit <glencoelho135@gmail.com>
2025-10-29 14:08:47 +0530

    reverted CI and pakage-json

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -6,34 +6,34 @@ test('checking the url', async ({page}) => {
   await expect(page).toHaveURL('http://localhost:3000/')
 })
 
-test('checking home page', async ({page}) => {
-  await page.goto('/')
-  await expect(page.getByRole('heading',{name: 'Welcome to React Email'})).toContainText('Welcome to React Email')
-})
+// test('checking home page', async ({page}) => {
+//   await page.goto('/')
+//   await expect(page.getByRole('heading',{name: 'Welcome to React Email'})).toContainText('Welcome to React Email')
+// })
 
-test('cheking for presence of notifications', async ({page}) => {
-  await page.goto('/')
-  await expect(page.locator('//h3')).toHaveText('notifications')
-})
+// test('cheking for presence of notifications', async ({page}) => {
+//   await page.goto('/')
+//   await expect(page.locator('//h3')).toHaveText('notifications')
+// })
 
-test('checking for presence of scripts', async ({page}) => {
-  await page.goto('/')
-  await page.locator('//h3').click()
-  for (const element of await page.locator('span.truncate.w-\\[calc\\(100\\%\\-1\\.25rem\\)\\]').all()) {
-  expect(element).toBeVisible()}
-  })
+// test('checking for presence of scripts', async ({page}) => {
+//   await page.goto('/')
+//   await page.locator('//h3').click()
+//   for (const element of await page.locator('span.truncate.w-\\[calc\\(100\\%\\-1\\.25rem\\)\\]').all()) {
+//   expect(element).toBeVisible()}
+//   })
 
-test('checking for screenshot light mode', async ({page}) => {
-  page.goto('/')
-  await page.locator('//h3').click()
-  await page.getByText('deposited-cycles').click()
-  await page.waitForTimeout(1000)
-  expect(await page.screenshot()).toMatchSnapshot(['screenshots','deposited-cycles.png'], {maxDiffPixels: 10})
+// test('checking for screenshot light mode', async ({page}) => {
+//   page.goto('/')
+//   await page.locator('//h3').click()
+//   await page.getByText('deposited-cycles').click()
+//   await page.waitForTimeout(1000)
+//   expect(await page.screenshot()).toMatchSnapshot(['screenshots','deposited-cycles.png'], {maxDiffPixels: 10})
 
-  await page.getByText('failed-deposit-cycles').click()
-  await page.waitForTimeout(1000)
-  expect(await page.screenshot()).toMatchSnapshot(['screenshots','failed-deposit-cycles.png'], {maxDiffPixels: 10})
-})
+//   await page.getByText('failed-deposit-cycles').click()
+//   await page.waitForTimeout(1000)
+//   expect(await page.screenshot()).toMatchSnapshot(['screenshots','failed-deposit-cycles.png'], {maxDiffPixels: 10})
+// })
 // test('checking for screenshot dark mode')
 
 
